328557aa4c237a98d61f879a7385a0cba70dc879
Choong Jin Ng <jin@complexsysteminc.com>
2025-05-28 17:11:52 -0600

    Make a copy of blinky for preprocess project

--- README.md
+++ README.md
@@ -4,3 +4,39 @@
 
 1. Using Visual Studio Code, open the directory at [`alif_src`](alif_src)
 2. Configure instructions according to README.md provided
+
+
+# How to create a new project `preprocess`
+1. Copy `blinky` project as it is and rename the directory to `preprocess`
+2. Inside [tasks.json](/alif_src/.vscode/tasks.json), add the following at line 15:
+```json
+        {
+            "label": "Clean all (including YML build files)",
+            "type": "shell",
+            "command": [
+                "cbuild ${command:cmsis-csolution.getSolutionPath} --clean;",
+                "rm -f ./blinky/*.cbuild.yml;",
+                "rm -f ./hello/*.cbuild.yml;",
+                "rm -f ./hello_rtt/*.cbuild.yml;",
+                "rm -f ./preprocess/*.cbuild.yml;"
+            ],
+            "windows": {
+                "command": [
+                    "cbuild ${command:cmsis-csolution.getSolutionPath} --clean;",
+                    "rm -Force ./blinky/*.cbuild.yml;",
+                    "rm -Force ./hello/*.cbuild.yml;",
+                    "rm -Force ./hello_rtt/*.cbuild.yml;",
+                    "rm -Force ./preprocess/*.cbuild.yml;"
+                ]
+            },
+            "problemMatcher": []
+        },
+```
+3. Inside [alif.csolution.yml](/alif_src/alif.csolution.yml), add the following line at line 45:
+```yml
+  projects:
+    - project: blinky/blinky.cproject.yml
+    - project: hello/hello.cproject.yml
+    - project: hello_rtt/hello_rtt.cproject.yml
+    - project: preprocess/preprocess.cproject.yml
+```
---
+++ alif_src/preprocess/main.c
@@ -0,0 +1,50 @@
+#include "RTE_Components.h"
+#include CMSIS_device_header
+
+#include "Driver_GPIO.h"
+#include "board.h"
+
+#define _GET_DRIVER_REF(ref, peri, chan) \
+    extern ARM_DRIVER_##peri Driver_##peri##chan; \
+    static ARM_DRIVER_##peri * ref = &Driver_##peri##chan;
+#define GET_DRIVER_REF(ref, peri, chan) _GET_DRIVER_REF(ref, peri, chan)
+
+GET_DRIVER_REF(gpio_b, GPIO, BOARD_LEDRGB0_B_GPIO_PORT);
+GET_DRIVER_REF(gpio_r, GPIO, BOARD_LEDRGB0_R_GPIO_PORT);
+
+int main (void)
+{
+    gpio_b->Initialize(BOARD_LEDRGB0_B_PIN_NO, NULL);
+    gpio_b->PowerControl(BOARD_LEDRGB0_B_PIN_NO, ARM_POWER_FULL);
+    gpio_b->SetDirection(BOARD_LEDRGB0_B_PIN_NO, GPIO_PIN_DIRECTION_OUTPUT);
+    gpio_b->SetValue(BOARD_LEDRGB0_B_PIN_NO, GPIO_PIN_OUTPUT_STATE_LOW);
+
+    gpio_r->Initialize(BOARD_LEDRGB0_R_PIN_NO, NULL);
+    gpio_r->PowerControl(BOARD_LEDRGB0_R_PIN_NO, ARM_POWER_FULL);
+    gpio_r->SetDirection(BOARD_LEDRGB0_R_PIN_NO, GPIO_PIN_DIRECTION_OUTPUT);
+    gpio_r->SetValue(BOARD_LEDRGB0_R_PIN_NO, GPIO_PIN_OUTPUT_STATE_LOW);
+
+#ifdef CORE_M55_HE
+    SysTick_Config(SystemCoreClock/25);
+#else
+    SysTick_Config(SystemCoreClock/50);
+#endif
+
+    while (1) __WFI();
+}
+
+void SysTick_Handler (void)
+{
+#ifdef CORE_M55_HE
+    gpio_b->SetValue(BOARD_LEDRGB0_B_PIN_NO, GPIO_PIN_OUTPUT_STATE_TOGGLE);
+#else
+    gpio_r->SetValue(BOARD_LEDRGB0_R_PIN_NO, GPIO_PIN_OUTPUT_STATE_TOGGLE);
+#endif
+}
+
+// Stubs to suppress missing stdio definitions for nosys
+#define TRAP_RET_ZERO  {__BKPT(0); return 0;}
+int _close(int val) TRAP_RET_ZERO
+int _lseek(int val0, int val1, int val2) TRAP_RET_ZERO
+int _read(int val0, char * val1, int val2) TRAP_RET_ZERO
+int _write(int val0, char * val1, int val2) TRAP_RET_ZERO
